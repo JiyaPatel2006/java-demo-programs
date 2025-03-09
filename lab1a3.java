d0991ec1920e903a9c3b721af2cc652bbfa38f62
JiyaPatel2006 <24010101214@darshan.ac.in>
2025-03-09 13:35:33 +0530

    Add files via upload

---
+++ lab1a3.java
@@ -0,0 +1,10 @@
+public class lab1a3{
+	public static void main(String[] args){
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+        int sum = a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("sum ="+sum); 
+	} 
+}
